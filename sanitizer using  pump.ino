5fc650e7a8b4e97b7d55da6338855847dd38ca14
Tech Rick <45835951+richu101@users.noreply.github.com>
2020-06-23 21:25:35 +0530

    Update sanitizer using  pump.ino

--- sanitizer using  pump.ino
+++ sanitizer using  pump.ino
@@ -1,46 +1,63 @@
+const int trigPin = 2;
+const int echoPin = 3;
+const int motorPin = 4;
+const int ledPin = 5;
+const int musicPin = 9;
 
-const int trigPin = 10;
-const int echoPin = 9;// defines variables
+int hand = 1;
+int triginterval = 1; // interval in milliseconds at which trigPin turns on
+
+unsigned long previousMillis = 0; // will store last time LED was updated
+int trigState = LOW;              //state of trigPin
+
 long duration;
-int distance;
-int pos = 0;
-int dis();
-void setup() {
-pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-pinMode(echoPin, INPUT); // Sets the echoPin as an Input
-Serial.begin(9600); // Starts the serial communication
-pinMode(13,OUTPUT);
+int d, distance = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);  // Sets the trigPin as an Output
+  pinMode(ledPin, OUTPUT);   // Sets the ledPin as an Output
+  pinMode(motorPin, OUTPUT); // Sets the buzzPin as an Output
+  pinMode(musicPin, OUTPUT); // Sets the buzzPin as an Output
+
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  digitalWrite(ledPin, LOW);
+  digitalWrite(musicPin, LOW); // Sets the buzzPin as an Output
+
+  digitalWrite(motorPin, LOW);
 }
-void loop() {
+
+void loop()
+{
 
 distance = dis();
-if(distance<50)
-{
-  digitalWrite(13,HIGH);
-  delay(1500);
-  digitalWrite(13,LOW);
-  while(distance>100)
-{
-digitalWrite(13,LOW);
-distance = dis();
-}
-}
+  if (distance < 50)
+  {
+    digitalWrite(ledPin, HIGH);
+    digitalWrite(motorPin, HIGH);
+    digitalWrite(musicPin, HIGH);
+    while (distance > 100)
+    {
+      digitalWrite(ledPin, LOW);
+    digitalWrite(motorPin, LOW);
+    digitalWrite(musicPin, LOW);
+      distance = dis();
+    }
+  }
+  
 }
 int dis()
 {
   digitalWrite(trigPin, LOW);
-delayMicroseconds(2);
-// Sets the trigPin on HIGH state for 10 micro seconds
-digitalWrite(trigPin, HIGH);
-delayMicroseconds(10);
-digitalWrite(trigPin, LOW);
-// Reads the echoPin, returns the sound wave travel time in microseconds
-duration = pulseIn(echoPin, HIGH);
-// Calculating the distance
-distance= duration*0.034/2;
-// Prints the distance on the Serial Monitor
-Serial.print("Distance: ");
-Serial.println(distance);
-return distance;
-}
+  delayMicroseconds(10);
+  distance = 0;
+  digitalWrite(trigPin, HIGH);
+  delay(50);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration * .0343) / 2;
 
+  Serial.println(distance);
+  return distance;
+}
