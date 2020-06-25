9b9d61308e928235d7f750cd99dc095610a80231
Tech Rick <45835951+richu101@users.noreply.github.com>
2020-06-25 17:33:15 +0530

    sanitizer using ir is added

---
+++ Pump sanitizer using IR sensor/ir pump.ino
@@ -0,0 +1,28 @@
+
+const int motorPin = 13;
+const int IRsensor = 2;
+
+void setup()
+{
+    Serial.begin(9600);
+    
+    pinMode(motorPin, OUTPUT); 
+    pinMode(IRsensor, INPUT); 
+    digitalWrite(motorPin, LOW);
+}
+
+void loop()
+{
+
+    if (digitalRead(2) == HIGH)
+    {
+        digitalWrite(motorPin, HIGH);
+        delay(1500);
+        digitalWrite(motorPin, LOW);
+        while (digitalRead(2) == LOW)
+        {
+
+            digitalWrite(motorPin, LOW);
+        }
+    }
+}
